e166c48cc011457d521ad7fdc4d176c41e804b5f
LuizEQueirogaLima <luizeduardoldas@gmail.com>
2026-06-19 02:03:51 -0300

    Melhorando a lógica do jogo e corrigindo a estrutura.

--- jogoAdivinhacao.py
+++ jogoAdivinhacao.py
@@ -1,19 +1,23 @@
 import time
 import random as rn
 
-finalizado = True
-
-class jogoAdivinhacao:
+class JogoAdivinhacao:
 
     def __init__(self):
         self.jogador = 0
+        self.numero_da_maquina = 0
 
     def mensagem_inicial(self):
         print("="*15,"Mini jogo de advinhação","="*15)
         print("  "*5,"Adivinhe um número entre 1 a 100","  "*5)
+        time.sleep(1)
+
+        
+    def jogada_d_maquina(self):
+        self.numero_da_maquina = rn.randint(1, 100)
 
     def jogada(self):
-
+        self.jogador = 0
         while True:
             try:
                 self.jogador = int(input("Tentativa: "))
@@ -23,72 +27,45 @@ class jogoAdivinhacao:
                 break
             except ValueError:
                 print("Erro, entrada inválida\n",'  ' *5,"Digite um número inteiro entre 1 e 100",'  ' *5)
-                time.sleep(1)
-        return(self.jogador)
-    
-    def jogada_d_maquina(self):
-        numero_d_maquina = rn.randint(1, 100)
-        return(numero_d_maquina)
-    
-    def partida(self,numero_d_maquina, resultado_tentativa):
-        while True:
-            numero_do_usuario_maior = 0
-            numero_do_usuario_menor = 0
-            numero_primo = True
-            finalizado = True
-            #print(f"jogada da maquina: {numero_d_maquina}")
-
-            if resultado_tentativa > numero_d_maquina:
-                numero_do_usuario_maior = resultado_tentativa - numero_d_maquina
-
-                if numero_do_usuario_maior % 2 == 0:
-                    print("\nDica número um: A difernça entre os números gera um número Par!!")
-                else:
-                    print("\nDica Número um: A diferença entre os dos números, gera um número ímpar!!")
-                
-                for i in range (2, numero_do_usuario_maior):
-                    if numero_do_usuario_maior % i == 0:
-                        print("\nDica número dois: A Diferença entre os dois números gera um número que não é primo!!")
-                        numero_primo = False
-                        break
-
-                if numero_primo:
-                    print("\nDica numero dois: A diferença entre os dois números gera um número Primo!!")
-
-                print("\n","=="*5,"Tente de novo!!","=="*5)
-                resultado_tentativa = self.jogada()
-
-            elif numero_d_maquina > resultado_tentativa:
-                numero_do_usuario_menor = numero_d_maquina - resultado_tentativa
-
-                if numero_do_usuario_menor % 2 == 0:
-                    print("\nDica número um: A difernça entre os números gera um número Par!!")
-                else:
-                    print("\nDica Número um: A diferença entre os dos números, gera um número ímpar!!")
-                
-                for i in range (2, numero_do_usuario_menor):
-                    if numero_do_usuario_menor % i == 0:
-                        print("\nDica número dois: A Diferença entre os dois números gera um número que não é primo!!")
-                        numero_primo = False
-                        break
-                if numero_primo:
-                    print("\nDica numero dois: A diferença entre os dois números gera um número Primo!!")
-                print("\n","=="*5,"Tente de novo!!","=="*5)
-                resultado_tentativa = self.jogada()
-
-            elif numero_d_maquina == resultado_tentativa:
-                print("Você acertou o número!!")
-                time.sleep(1)
-                print("fechando o programa!!!")
-                time.sleep(1)
-                finalizado = False
-                break
-        return(finalizado)
+                time.sleep()
         
-while finalizado:
+    def partida(self):
+        numero_primo = True
+        diferenca = abs(self.numero_da_maquina - self.jogador)
+        #print(self.numero_da_maquina)
+        
+        if self.numero_da_maquina == self.jogador:
+            print("Você acertou o número!!")
+            time.sleep(1)
+            print("Fechando o programa!!!")
+            time.sleep(1)
+            return True 
 
-    jogo = jogoAdivinhacao()
-    jogo.mensagem_inicial()
-    numero_d_maquina = jogo.jogada_d_maquina()
-    resultado_tentativa = jogo.jogada()
-    finalizado = jogo.partida(numero_d_maquina,resultado_tentativa)
+        if diferenca % 2 == 0:
+            print("Dica número um: A diferença entre os números gera um número Par!!")
+        else:
+            print("Dica Número um: A diferença entre os números gera um número Ímpar!!")
+            
+        if diferenca == 1:
+            print("Dica número dois: A diferença entre os dois números gera um número que não é primo!!")
+        else:
+            for i in range(2, diferenca):
+                if diferenca % i == 0:
+                    numero_primo = False
+                    break
+                    
+            if numero_primo:
+                print("Dica numero dois: A diferença entre os dois números gera um número Primo!!")
+            else:
+                print("Dica número dois: A Diferença entre os dois números gera um número que não é primo!!")
+                
+        print("\n", "=-="*4, "Tente de novo!!", "=-="*4)
+
+jogo = JogoAdivinhacao()
+jogo.mensagem_inicial()
+jogo.jogada_d_maquina()
+
+jogo_vencido = False
+while not jogo_vencido:
+    jogo.jogada()
+    jogo_vencido = jogo.partida()
