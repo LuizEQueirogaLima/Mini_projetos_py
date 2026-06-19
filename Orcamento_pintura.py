327b0c6a1636b0642be2cff96074e0f6011ea9a8
LuizEQueirogaLima <luizeduardoldas@gmail.com>
2026-06-19 12:28:35 -0300

    Melhoria na validação de entrada e ajustes na lógica de cálculo de área para pintura.

--- Orcamento_pintura.py
+++ Orcamento_pintura.py
@@ -2,7 +2,6 @@ import time
 import math
 
 #Sistema calculador de área para pintura
-
 # Passo 1: Analisar quantidade e tamanhos diferentes de paredes
 # Passo 2: Entrada de Altura, largura 
 # Passo 3: Tamanho da lata (litros) e Rendimento por litro
@@ -21,7 +20,11 @@ class Parede:
             if quant_paredes.isdigit():
                 quant_paredes = int(quant_paredes)
                 if quant_paredes > 10:
-                    print("quantidade muito grande de paredes para serem calculadas\nPorfavor coloque uma quantidade a baixo de 10")
+                    print("quantidade muito grande de paredes para calcular\nPorfavor coloque uma quantidade que seja igual ou a baixo de 10\n")
+                    time.sleep(1)
+                    continue
+                elif quant_paredes <= 0:
+                    print("Erro.. a quantidade não pode ser 0 nem um número negativo.. \nTente de novo")
                     continue
                 print("Registrado!!\n")
                 time.sleep(1)
@@ -44,30 +47,29 @@ class Parede:
                 while True:
                     try:
                         self.medidas.append(
-                            float(input("Digite a altura da parede: ").replace(',','.')))
+                            float(input("Digite a altura da parede M: ").replace(',','.')))
                         self.medidas.append(
-                            float(input("Digite a largura da parede: ").replace(',','.')))
-                        self.medidas.append(quant_paredes)
+                            float(input("Digite a largura da parede M: ").replace(',','.')))
+                        #self.medidas.append(quant_paredes)
                         print("\n","  "*10,"Informações registradas!!","  "*10,"\n")
                         time.sleep(1)
-
                         self.p_Tamanho_diferente = False
                         break
-                    
+                         
                     except ValueError:
                         print("Erro, uma das informações foi inválida, digite novamente\n")
                         time.sleep(1)
                 break
             elif verifica_as_paredes == "N":
                 print("As paredes são de tamanhos diferentes\n")
-                medidas = []
-                for cont in range (quant_paredes):
+                for cont in range (1,quant_paredes + 1):
+                    medidas = []
                     while True:
                         try:
                             medidas.append(
-                                float(input("Digite a altura da parede: ").replace(',','.')))
+                                float(input(f"Digite a altura em metros da parede N°{cont}: ").replace(',','.')))
                             medidas.append(
-                                float(input("Digite a largura da parede: ").replace(',','.')))
+                                float(input(f"Digite a largura em metros da parede N°{cont}: ").replace(',','.')))
                             print("\n","  "*10,"Informações registradas!!","  "*10,"\n")
                             
                             self.lista_d_paredes[cont] = medidas
@@ -79,6 +81,8 @@ class Parede:
                 break
             else:
                 print("Erro... você deve digitar [S] para sim ou [N] Para Não\n")
+
+        return(self.p_Tamanho_diferente,self.medidas,self.lista_d_paredes)        
         
 class TipoTinta:
     def __init__(self):
@@ -90,41 +94,43 @@ class TipoTinta:
             try:
                 self.volume = float(input("Digite o volume de litros da lata de tinta: ").replace(',','.'))
                 self.rendiment_p_litro = float(input("Digite o volume de rendimento da tinta: ").replace(',','.'))
-                print("Informações registradas")
+                if self.volume <=0 or self.rendiment_p_litro <= 0:
+                    print("Por porfavor digite um número positivo para a verificação..\n")
+                    time.sleep(1)
+                    continue
+                print("\n","  "*10,"Informações registradas!!","  "*10,"\n")
                 time.sleep(1)
                 break
             except ValueError:
                 print("Erro, uma das informações foi inválida, digite novamente\n")
                 time.sleep(1)
+        return self.volume,self.rendiment_p_litro
             
 class Calculador:
     def __init__(self):
         self.quantlatas = 0
     
-    def calcular (self):
-        if minha_parede.p_Tamanho_diferente == True: 
+    def calcular (self, p_Tamanho_diferente, medidas, lista_de_paredes, quant_paredes):
+        if p_Tamanho_diferente == True: 
             area_total = 0
-            contador_das_paredes = 0
 
-            for indice, info in minha_parede.lista_d_paredes.items():# ver esse indice
+            for indice, info in lista_de_paredes.items():
                
                 area_total += info[0] * info[1]
-                contador_das_paredes += 1
                 
             print("\nCalculando informações\n")
             time.sleep(1)
             
-            print(f"Foram {contador_das_paredes} paredes de tamanhos diferentes, segue status completo de calculo.\n")
+            print(f"Contamos um numero de n°: {quant_paredes} paredes de tamanhos diferentes, segue status completo de calculo.\n")
             time.sleep(1)
-            self.otimizacao(area_total)
             
-        elif minha_parede.p_Tamanho_diferente == False:
+            
+        elif p_Tamanho_diferente == False:
             area_total = 0
 
-
-            if minha_parede.medidas[2] > 1:
+            if quant_paredes > 1:
                 time.sleep(1)
-                print(f"\nTendo a quantidade de paredes: {minha_parede.medidas[2]}, segue o calculo\n")
+                print(f"\nTendo a quantidade de paredes: {quant_paredes}, segue o calculo\n")
                 time.sleep(1)
             else:
                 time.sleep(1)
@@ -132,21 +138,21 @@ class Calculador:
                 time.sleep(1)
             
 
-            area_total = minha_parede.medidas[0] * minha_parede.medidas[1] * minha_parede.medidas[2]
+            area_total = medidas[0] * medidas[1] * quant_paredes
              
-            self.otimizacao(area_total)
+        return area_total
 
             
-    def otimizacao (self,area_total):
+    def otimizacao (self,area_total, volume_da_lata, rendimento_litro):
             sobra_em_litros = 0
             quant_latas = 0
             capacidade_d_compra = 0
             
-            quant_latas = math.ceil(area_total / (tinta_d_parede.volume * tinta_d_parede.rendiment_p_litro))
+            quant_latas = math.ceil(area_total / (volume_da_lata * rendimento_litro))
         
-            capacidade_d_compra = quant_latas * (tinta_d_parede.volume * tinta_d_parede.rendiment_p_litro)
+            capacidade_d_compra = quant_latas * (volume_da_lata * rendimento_litro)
 
-            sobra_em_litros = (capacidade_d_compra - area_total) / tinta_d_parede.rendiment_p_litro
+            sobra_em_litros = (capacidade_d_compra - area_total) / rendimento_litro
             
             print(f"Minha área total é de {area_total:.2f}M²\n")
             print(f"A quantidade de latas que você precisa são: {quant_latas} unidades\n")
@@ -158,12 +164,12 @@ class Calculador:
         
 print("=-="*15,"Programa simples calculador","=-="*15)
 minha_parede = Parede()
-quant_paredes = minha_parede.quantidade_d_paredes()
-minha_parede.info_parede(quant_paredes)
-
 tinta_d_parede = TipoTinta()
-tinta_d_parede.tinta_gasta()
-
 inp_calculo = Calculador()
-inp_calculo.calcular()
+
+quant_paredes = minha_parede.quantidade_d_paredes()
+p_Tamanho_diferente, medidas, lista_de_paredes = minha_parede.info_parede(quant_paredes)
+volume_da_lata, rendimento_litro = tinta_d_parede.tinta_gasta()
+area_total = inp_calculo.calcular(p_Tamanho_diferente, medidas, lista_de_paredes, quant_paredes)
+inp_calculo.otimizacao(area_total,volume_da_lata, rendimento_litro)
 
